ec23b63d14fe3d0149f2e92cc146bebe74cbed65
Kai Chen <kaichen120@gmail.com>
2020-02-26 23:28:00 -0800

    Bunch of changes uhhh

--- src/override/override.js
+++ src/override/override.js
@@ -1,6 +1,6 @@
 function initPage() {
     initClock();
-    setMessageUnderClock()
+    setMessageUnderClock();
 }
 
 function initClock() {
@@ -9,24 +9,22 @@ function initClock() {
 }
 
 function setMessageUnderClock() {
-    // TODO: add to locales instead of string
     let message = "Hello.";
     const hours = getCurrentTime()[0];
 
-
-    // TODO: do something else other than this to handle weird cases
-    // TODO: add into translatable text!!
     if(hours > 24) {
-        message = "Have a good day."
+        message = "Have a good day"
     } else if (hours >= 18) {
-        message = "Good evening."
+        message = "Good evening"
     } else if (hours < 6) {
-        message = "Have a good night."
+        message = "Have a good night"
     } else if (hours >= 12) {
-        message = "Good afternoon."
+        message = "Good afternoon"
     } else if (hours >= 6) {
-        message = "Good morning."
+        message = "Good morning"
     }
+
+    // message += ", Kai.";
 
     document.getElementById("message-under-clock").innerHTML = message;
 }
@@ -41,21 +39,21 @@ function getCurrentTime() {
     return [h, m];
 }
 
-// TODO: better naming
 function doTimeCount() {
     let time = getCurrentTime();
     setTimeOnPage(time);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    if (i < 10) {
+        i = "0" + i
+    }
     return i;
 }
 
 function setTimeOnPage(time) {
     document.getElementById('clock').innerHTML =  time[0] + ":" + time[1];
 }
-
 
 function ready(fn) {
     if (document.readyState !== 'loading'){
