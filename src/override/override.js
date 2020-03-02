2e8071da7c5aaa194e96c25ba02d510f2a4cbe89
Kai Chen <kaichen120@gmail.com>
2020-03-02 12:20:13 -0800

    Lint

--- src/override/override.js
+++ src/override/override.js
@@ -1,76 +1,77 @@
 function initPage() {
-    startClock();
-    setMessageUnderClock();
+  startClock();
+  setMessageUnderClock();
 
-    setTimeout(fadeInBackground, 100);
-    document.getElementById("settings-cog").addEventListener("click", openSettingsMenu);
+  setTimeout(fadeInBackground, 100);
+  document
+    .getElementById("settings-cog")
+    .addEventListener("click", openSettingsMenu);
 }
 
 function openSettingsMenu(e) {
-    alert("settings");
+  alert("settings");
 }
 
 function fadeInBackground() {
-    document.getElementById("body-wrapper").style.display = "block";
+  document.getElementById("body-wrapper").style.display = "block";
 }
 
 function startClock() {
-    doTimeCount();
-    setTimeout(startClock, 500);
+  doTimeCount();
+  setTimeout(startClock, 500);
 }
 
 function setMessageUnderClock() {
-    let message = "Hello.";
-    const hours = getCurrentTime()[0];
+  let message = "Hello.";
+  const hours = getCurrentTime()[0];
 
-    if(hours > 24) {
-        message = "Have a good day"
-    } else if (hours >= 18) {
-        message = "Good evening"
-    } else if (hours < 6) {
-        message = "Have a good night"
-    } else if (hours >= 12) {
-        message = "Good afternoon"
-    } else if (hours >= 6) {
-        message = "Good morning"
-    }
+  if (hours > 24) {
+    message = "Have a good day";
+  } else if (hours >= 18) {
+    message = "Good evening";
+  } else if (hours < 6) {
+    message = "Have a good night";
+  } else if (hours >= 12) {
+    message = "Good afternoon";
+  } else if (hours >= 6) {
+    message = "Good morning";
+  }
 
-    message += ".";
+  message += ".";
 
-    document.getElementById("message-under-clock").innerHTML = message;
+  document.getElementById("message-under-clock").innerHTML = message;
 }
 
-
 function getCurrentTime() {
-    const today = new Date();
-    const h = today.getHours();
-    let m = today.getMinutes();
-    m = checkTime(m);
+  const today = new Date();
+  const h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
 
-    return [h, m];
+  return [h, m];
 }
 
 function doTimeCount() {
-    setTimeOnPage(getCurrentTime());
+  setTimeOnPage(getCurrentTime());
 }
 
 function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
-    }
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
 function setTimeOnPage(time) {
-    document.getElementById('clock').innerHTML =  time[0] + ":" + time[1];
+  document.getElementById("clock").innerHTML = time[0] + ":" + time[1];
 }
 
 function ready(fn) {
-    if (document.readyState !== 'loading'){
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
 ready(initPage);
