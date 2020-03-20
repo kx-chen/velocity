cc72a7e2b819d0ce4a14d5508db77c3c33e6a721
Kai Chen <kaichen120@gmail.com>
2020-03-20 15:11:09 -0700

    VEL-7 Add basic menu popup
    Blank, doesn't use preact yet.

--- src/js/index.js
+++ src/js/index.js
@@ -27,7 +27,20 @@ function promptUserForNameIfNeeded() {
 }
 
 function openSettingsMenu(e) {
-  alert("settings");
+  let modal = document.getElementById("myModal");
+  let span = document.getElementsByClassName("close")[0];
+
+  modal.style.display = "block";
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
 function fadeInBackground() {
